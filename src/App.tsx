618c748cfd8a94d8e0048cdaa65a7811d8b9ee69
gregory133 <jmarc134@uottawa.ca>
2023-10-24 15:08:02 -0400

    done with the rough Board component

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,18 @@
 import React, { useRef, useState } from 'react';
 import Board from './components/Board';
 import './styles/App.css'
+import * as cg from 'chessground/types.js';
+
 function App() {
 
   const divRef=useRef(null)
 
   return (
     <div ref={divRef} style={{width: '100vw', height: '100vh'}}>
-      <Board parentRef={divRef} lastMove={null} 
-      fen={'2k5/8/8/8/8/8/8/2K5 w - - 0 1'} 
-      isMovable={true} orientation='white'
+      <Board parentRef={divRef} lastMove={undefined} 
+      fen={'rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1'} 
+      colorPlayerCanControl={'white'} 
+      orientation='white'
        afterMove={()=>{}} />
     </div>
    
--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -10,9 +10,9 @@ import "../styles/chessground.cburnett.css";
 import ResponsiveSquare from './ResponsiveSquare';
 
 interface Props{
-	isMovable:boolean
+	colorPlayerCanControl: cg.Color|null
 	fen:string
-	lastMove: null|cg.Key[]
+	lastMove: undefined|cg.Key[]
 	orientation: cg.Color
 	afterMove: ()=>void
 	parentRef:React.MutableRefObject<null|HTMLElement>
@@ -22,6 +22,13 @@ export default function Board(props:Props) {
 
 	const [length, setLength]=useState(0)
 	const chess=new Chess(props.fen)
+
+	const SQUARES:string[]=['a8' , 'b8' , 'c8' , 'd8' , 'e8' , 'f8' , 'g8' , 'h8' , 
+	'a7' , 'b7' , 'c7' , 'd7' , 'e7' , 'f7' , 'g7' , 'h7' , 'a6' , 'b6' , 'c6' , 'd6' , 
+	'e6' , 'f6' , 'g6' , 'h6' , 'a5' , 'b5' , 'c5' , 'd5' , 'e5' , 'f5' , 'g5' , 'h5' , 
+	'a4' , 'b4' , 'c4' , 'd4' , 'e4' , 'f4' , 'g4' , 'h4' , 'a3' , 'b3' , 'c3' , 'd3' , 
+	'e3' , 'f3' , 'g3' , 'h3' , 'a2' , 'b2' , 'c2' , 'd2' , 'e2' , 'f2' , 'g2' , 'h2' , 
+	'a1' , 'b1' , 'c1' , 'd1' , 'e1' , 'f1' , 'g1' , 'h1']
 
 	/**
 	 * 
@@ -49,13 +56,52 @@ export default function Board(props:Props) {
 	}
 
 	/**
+	 * @param chess chess object encoding the current position
+	 * @returns an object encapsulating the possible destinations of 
+	 * each piece for chessground
+	 */
+	function getDests(chess:Chess, colorPlayerCanControl:cg.Color|null)
+	:Map<cg.Key, cg.Key[]>{
+		
+		let dests:Map<cg.Key, cg.Key[]>=new Map();
+		let colorToMove:cg.Color=chess.turn()=='w' ? 'white' : 'black'
+
+		if (colorPlayerCanControl==null || colorToMove!=colorPlayerCanControl){
+			return dests
+		}
+	
+		SQUARES.forEach(square=>{
+			const moves = chess.moves({square: square as Square, verbose: true});
+			if (moves.length){
+				dests.set(square as cg.Key, moves.map(
+					(move:any) => move.to)
+				);
+			}
+		})
+		
+		return dests
+	}
+
+	/**
 	 * 
 	 * @returns the config object required by Chessground
 	 */
 	function getConfig():Config{
 
+		const turnColor:cg.Color = props.fen.split(' ')[1]=='w'
+		? 'white' : 'black'
 		
-		return {coordinates:false}
+		return {
+			fen: props.fen,
+			orientation: props.orientation,
+			coordinates:false,
+			turnColor: turnColor,
+			lastMove: props.lastMove,
+			movable: {
+				free: false,
+				dests: getDests(chess, props.colorPlayerCanControl)
+			}
+		}
 	}
 
 	useEffect(()=>{
