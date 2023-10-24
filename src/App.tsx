b65d4e4e6d9f177855b35058c5fbd5a6819f7f95
gregory133 <jmarc134@uottawa.ca>
2023-10-24 01:29:40 -0400

    responsive chess board bug when zooming out

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,18 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
 import Board from './components/Board';
 
 function App() {
+
+  const divRef=useRef(null)
+
   return (
-    <div style={{width: 600, height: 600}}>
- <Board/>
+    <div ref={divRef} style={{width: '90vw', height: '90vh'}}>
+      <Board parentRef={divRef} lastMove={null} 
+      fen={'2k5/8/8/8/8/8/8/2K5 w - - 0 1'} 
+      isMovable={true} orientation='white'
+       afterMove={()=>{}} />
     </div>
    
-    // <div>hello</div>
   );
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -6,7 +6,10 @@ const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <React.StrictMode>
+
+  <div>
     <App/>
-  </React.StrictMode>
+  </div>
+  
+
 );
