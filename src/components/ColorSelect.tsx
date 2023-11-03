9d633151bfc97311398cdd5c107b3537b1e43f6f
gregory133 <jmarc134@uottawa.ca>
2023-11-03 12:09:50 -0400

    added select colors, not hooked up yet

--- src/components/ColorSelect.tsx
+++ src/components/ColorSelect.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styles from '../styles/ColorSelect.module.css'
 
 interface Button{
@@ -7,16 +7,33 @@ interface Button{
   hoverText: string
 }
 
+enum Color{
+  WHITE, ANY, BLACK
+}
+
 export default function ColorSelect() {
 
+  const [selectedIndex, setSelectedIndex]=useState<Color>(Color.ANY)
+  
+
   const buttons: Button[]=[
-    {bgImage: `url(${process.env.PUBLIC_URL}/images/white.png)`, onClick:()=>{},
+    {bgImage: `url(${process.env.PUBLIC_URL}/images/white.png)`, onClick:()=>{
+      onClickChooseColorButton(Color.WHITE)
+    },
     hoverText: 'Play as White'},
-    {bgImage: `url(${process.env.PUBLIC_URL}/images/blackwhite.png)`, onClick:()=>{},
+    {bgImage: `url(${process.env.PUBLIC_URL}/images/blackwhite.png)`, onClick:()=>{
+      onClickChooseColorButton(Color.ANY)
+    },
     hoverText: 'Play as Any Color'},
-    {bgImage: `url(${process.env.PUBLIC_URL}/images/black.png)`, onClick:()=>{},
+    {bgImage: `url(${process.env.PUBLIC_URL}/images/black.png)`, onClick:()=>{
+      onClickChooseColorButton(Color.BLACK)
+    },
     hoverText: 'Play as Black'},
   ]
+
+  function onClickChooseColorButton(color:Color){
+    setSelectedIndex(color)
+  }
 
   return (
     <div className={styles.parent}>
@@ -24,9 +41,13 @@ export default function ColorSelect() {
 
         {
           buttons.map((button:Button, key:number)=>{
+            const border=selectedIndex==key 
+              ? ('2px solid green') 
+              : ('2px solid transparent')
 
             return (
-              <div key={key} className={styles.button}>
+              <div title={button.hoverText} key={key} className={styles.button} 
+              style={{border: border}} onClick={button.onClick}>
                 <div className={styles.buttonImage} style={{
                   backgroundImage: button.bgImage
                 }}/>
