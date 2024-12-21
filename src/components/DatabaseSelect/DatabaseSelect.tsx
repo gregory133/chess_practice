dfc62e0492574acf55f5e858f500284a135a8353
gregory133 <jmarc134@uottawa.ca>
2024-12-20 22:42:46 -0500

    added masters database options select

--- src/components/DatabaseSelect/DatabaseSelect.tsx
+++ src/components/DatabaseSelect/DatabaseSelect.tsx
@@ -4,8 +4,15 @@ import styles from './DatabaseSelect.module.scss'
 import { useChessStore } from '../../stores/chessStore'
 import { Database } from '../../api/DBApi'
 import Modal from 'react-modal';
-import DatabaseSettingsModal from '../DatabaseSettingsModal/DatabaseSettingsModal'
 import { Dictionary } from 'typescript-collections'
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { DatePicker } from '@mui/x-date-pickers/DatePicker';
+import { DesktopDatePicker } from '@mui/x-date-pickers/DesktopDatePicker'
+import { styled } from '@mui/material'
+import YearPicker from '../YearPicker/YearPicker'
+import { YEAR_LOWER_BOUND, YEAR_UPPER_BOUND } from '../../constants/MastersDatabase'
+import { useDatabaseSettingsStore } from '../../stores/databaseSettingsStore'
 
 export default function DatabaseSelect() {
 
@@ -14,13 +21,19 @@ export default function DatabaseSelect() {
 
   const [selectedId, setSelectedId]=useState<string>(selectedDatabase)
   const [isDatabaseModalOpen, setIsDatabaseModalOpen]=useState(false)
+  const [testDate, setTestDate] = useState<Date>(new Date(Date.now()))
+
+  const mastersOptions = useDatabaseSettingsStore(state=>state.mastersOptions)
+  const setMastersOptions = useDatabaseSettingsStore(state=>state.setMastersOptions)
+
+  const allDatabases : Database[] = ['lichess', 'masters', 'player']
 
   const buttonNameDict = new Dictionary<string, string>();{
     buttonNameDict.setValue('masters', 'Master Database')
     buttonNameDict.setValue('lichess', 'Lichess Database')
     buttonNameDict.setValue('player', 'Lichess Player Database')
   }
-  
+
   const buttons: Button[]=[
     {id: 'masters', bgImage: `${process.env.PUBLIC_URL}/images/masters.png`,
     onClick:()=>{
@@ -51,29 +64,63 @@ export default function DatabaseSelect() {
     setIsDatabaseModalOpen(true)
   }
 
+  function onMastersSinceYearChange(year:number){
+    setMastersOptions({since: year, until: mastersOptions.until})
+  }
+
+  function onMastersUntilYearChange(year:number){
+    setMastersOptions({since: mastersOptions.since, until: year})
+  }
+
   useEffect(()=>{
-    // console.log(selectedDatabase);
-  }, [selectedDatabase])
+    console.log(mastersOptions)
+  }, [mastersOptions])
 
   return (
-    <div className={styles.parent}>
-      <div onClick={onClickSettingsButton} className={styles.settingsButton}/>
-      <p>Select Database: </p>
-      <div className={styles.container}>
-        {
-          buttons.map((button:Button, key:number)=>{
-            return(
-              <div key={key} className={styles.button}>
-                <OptionButton button={button} 
-                isHighlighted={button.id==selectedId}/>
-                <p>{buttonNameDict.getValue(button.id)}</p>
+    <div className={styles.main}>
+      <div className={styles.parent}>
+        <div onClick={onClickSettingsButton} className={styles.settingsButton}/>
+        <p>Select Database: </p>
+        <div className={styles.container}>
+          {
+            buttons.map((button:Button, key:number)=>{
+              return(
+                <div key={key} className={styles.button}>
+                  <OptionButton button={button} 
+                  isHighlighted={button.id==selectedId}/>
+                  <p>{buttonNameDict.getValue(button.id)}</p>
+                </div>
+              )
+            })
+          }
+        </div>
+      </div>
+      {
+        allDatabases.map((database, key)=>{
+          if (database == 'masters'){
+            return (
+              <div className={styles.mastersDatepickers}>
+                <YearPicker onChange={onMastersSinceYearChange} label='since' currentYear={mastersOptions.since} 
+                yearOrder='asc' defaultYear={YEAR_LOWER_BOUND} minYear={YEAR_LOWER_BOUND} 
+                maxYear={mastersOptions.until}/>
+
+                <YearPicker onChange={onMastersUntilYearChange} label='until' currentYear={mastersOptions.until} 
+                yearOrder='desc' defaultYear={YEAR_UPPER_BOUND} minYear={mastersOptions.since} 
+                maxYear={YEAR_UPPER_BOUND}/>
               </div>
             )
-          })
-        }
-      </div>
 
-      <DatabaseSettingsModal isOpen={isDatabaseModalOpen} setIsOpen={setIsDatabaseModalOpen}/>
+          }
+          else if (database == 'player'){
+
+          }
+          else if (database == 'lichess'){
+            
+          }
+          return <></>
+        })
+      }
     </div>
+    
   )
 }
