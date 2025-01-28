bf9748f1cc28f2f7944ba6188561551002640a9f
gregory133 <jmarc134@uottawa.ca>
2025-01-27 23:50:24 -0500

    added toggle for database moves

--- src/components/MovesBar/MovesBar.tsx
+++ src/components/MovesBar/MovesBar.tsx
@@ -1,37 +1,43 @@
 import React, { useEffect, useState } from 'react'
 import styles from './MovesBar.module.scss'
-import { Dictionary } from 'typescript-collections'
-import Playrate from '../../classes/Playrate'
-import Winrate from '../../classes/Winrate'
 import WinrateBar from '../Sidebar/WinrateBar/WinrateBar'
 import { fetchDB, getPlayrateFromDB, getSanListFromDB } from '../../api/DBApi'
-import MastersDatabaseSettings from '../../classes/DatabaseSettings/MastersDatabaseSettings'
-import DatabaseSettings from '../../interfaces/DatabaseSettings'
 import { useChessStore } from '../../stores/chessStore'
+import Switch from '@mui/material/Switch';
 
 export default function MovesBar() {
 
   const playrate = useChessStore(state=>state.playrate)
-  
+  const [isDisabled, setIsDisabled] = useState(false)
+
+  function onChangeDisableSwitch(event:any, checked:boolean){
+    setIsDisabled(!checked)
+  }
+
   return (
     <div className={styles.main}>
         <div className={styles.bar}>
           <div className={styles.title}>
             Database Moves
+            <Switch onChange={onChangeDisableSwitch} defaultChecked/>
           </div>
           <div className={styles.playrateList}>
             {
-              playrate.getDict().keys().map((move:string, index:number)=>{
-                return (
-                  <div key={index} className={styles.playrateListItem}>
-                    <span className={styles.move}>{move}</span>
-                    <span className={styles.playrate}>
-                      {Math.round(playrate.getDict().getValue(move)!.playrate * 10000) / 100}%
-                    </span>
-                    <WinrateBar winrate={playrate.getDict().getValue(move)!.winrate}/>
-                  </div>
-                )
-              })
+              isDisabled 
+                ? null
+                : (
+                  playrate.getDict().keys().map((move:string, index:number)=>{
+                    return (
+                      <div key={index} className={styles.playrateListItem}>
+                        <span className={styles.move}>{move}</span>
+                        <span className={styles.playrate}>
+                          {Math.round(playrate.getDict().getValue(move)!.playrate * 10000) / 100}%
+                        </span>
+                        <WinrateBar winrate={playrate.getDict().getValue(move)!.winrate}/>
+                      </div>
+                    )
+                  })
+                ) 
             }
           </div>
         </div>
