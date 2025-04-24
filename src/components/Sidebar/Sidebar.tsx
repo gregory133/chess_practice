19ff6c0606a59d43f1a436fca60ebd6073db7650
gregory133 <jmarc134@uottawa.ca>
2025-04-24 16:57:50 -0400

    colors to top bar

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -1,4 +1,4 @@
-import { useState, ReactElement } from 'react'
+import { useState, ReactElement, useEffect } from 'react'
 import InfoIcon from '@mui/icons-material/Info';
 import styles from './Sidebar.module.scss'
 
@@ -16,8 +16,14 @@ interface TopBarOption{
 }
 
 export default function Sidebar() {
+
+  //colors
+  const UNHIGHLIGHTED_COLOR = '#26231e'
+  const HIGHTLIGHTED_COLOR = '#545454'
+  const HOVERED_COLOR = '#1e1d18'
   
   const [selectedTopbarIcon, setSelectedTopbarIcon] = useState<string>('Database')
+  const [topBarButtonsHoverState, setTopBarButtonsHoverState] = useState<boolean[]>([false, false, false])
 
   const topBarOptionsSx = {margin: '0 0.5rem 0 0', width: '2.5rem', height: '2.5rem'}
 
@@ -47,20 +53,43 @@ export default function Sidebar() {
     setSelectedTopbarIcon(option)
   }
 
+  function onMouseEnterTopBarButton(key:number){
+    let newTopBarButtonsHoverState = topBarButtonsHoverState
+    newTopBarButtonsHoverState[key] = true
+    setTopBarButtonsHoverState([...newTopBarButtonsHoverState])
+  }
+
+  function onMouseLeaveTopBarButton(key:number){
+    let newTopBarButtonsHoverState = topBarButtonsHoverState
+    newTopBarButtonsHoverState[key] = false
+    setTopBarButtonsHoverState([...newTopBarButtonsHoverState])
+  }
+
   return (
 
     <div className={styles.main}>
-        <div className={styles.topOptions}>
+        <div className={styles.topOptions} >
           {
             topBarOptions.map((option, key)=>{
-              let backgroundColor = '#26231e'
+
+              let backgroundColor = UNHIGHLIGHTED_COLOR
+
               if (option.name == selectedTopbarIcon){
-                backgroundColor = '#545454'
+                backgroundColor = HIGHTLIGHTED_COLOR
               }
+              else if (topBarButtonsHoverState[key]){
+                backgroundColor = HOVERED_COLOR
+              }
+
+
               return (
-                <div key={key} className={styles.topOptionButton}
+                <div className={styles.topOptionButton} key={key} 
+                
+                onMouseEnter={()=>onMouseEnterTopBarButton(key)}            
+                onMouseLeave={()=>onMouseLeaveTopBarButton(key)}
+
                 onClick={()=>onClickTopbarOption(option.name)}
-                 style={{backgroundColor: backgroundColor}} >
+                 style={{backgroundColor: backgroundColor}}>
                   {option.icon}
                   {option.name}
                 </div>
