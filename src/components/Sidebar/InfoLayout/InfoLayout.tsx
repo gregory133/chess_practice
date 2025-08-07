abb526e1b20d711b5ceb96323ece116b748ef5c8
gregory133 <jmarc134@uottawa.ca>
2025-08-06 23:55:21 -0400

    fixed split second visual glitch for info

--- src/components/Sidebar/InfoLayout/InfoLayout.tsx
+++ src/components/Sidebar/InfoLayout/InfoLayout.tsx
@@ -9,23 +9,23 @@ import ContentCopyIcon from '@mui/icons-material/ContentCopy';
 
 export default function InfoLayout() {
 
-    const [openingName, setOpeningName] = useState<string>('')
-    const [numGamesInDB, setNumGamesInDB] = useState<number>(0)
-    const [numPossibleMoves, setNumPossibleMoves] = useState<number>(0)
-    const [winrate, setWinrate] = useState<Winrate>(new Winrate(0, 0))
+    const [openingName, setOpeningName] = useState<string|null>(null)
+    const [numGamesInDB, setNumGamesInDB] = useState<number|null>(null)
+    const [numPossibleMoves, setNumPossibleMoves] = useState<number|null>(null)
+    const [winrate, setWinrate] = useState<Winrate|null>(null)
 
     const {database} = useContext(SettingsContext)!
     const {fen} = useContext(BoardContext)!
 
     useEffect(()=>{
+        console.log(openingName)
+    }, [openingName])
 
+    useEffect(()=>{
         DatabaseAPI.getInstance().getPositionInfo(fen, database)
         .then(response=>{
 
-            const openingName = response.openingName
-            if (openingName){
-                setOpeningName(openingName)
-            }
+            if (response.openingName) setOpeningName(response.openingName); else if (!openingName) setOpeningName('')
             setNumGamesInDB(response.numGamesInDatabase)
             setNumPossibleMoves(response.numPossibleMoves)
             setWinrate(response.winrate)
@@ -42,43 +42,57 @@ export default function InfoLayout() {
     function onClickCopyFen(){
         navigator.clipboard.writeText(fen)  
     }
-    
+
+    /**
+     * returns true if the component has been fully rendered and contains proper values
+     */
+    function isComponentFullyLoaded() : boolean{
+        return openingName!=null && numGamesInDB!=null && numPossibleMoves != null && winrate != null
+    }
 
     return (
-        <div className={styles.main}>
+        <>
+        {
+            !isComponentFullyLoaded() ? null : (
+                <div className={styles.main}>
 
-            <div className={styles.openingName}>
-                {openingName}
-                <div className={styles.winrateBarContainer}>
-                    <WinrateBar winrate={winrate}/>
+                    <div className={styles.openingName}>
+                        {openingName}
+                        <div className={styles.winrateBarContainer}>
+                            <WinrateBar winrate={winrate}/>
+                        </div>
+                    </div>
+
+                    <div className={styles.separator}/>
+
+                    <div className={styles.dbInfo}>
+
+                        <div className={styles.numGamesInDB}>
+                            {prettyPrintNumber(numGamesInDB!)}  game(s) in database
+                        </div>
+
+                        •
+
+                        <div className={styles.numMovesInDB}>
+                            {numPossibleMoves} possible move(s) in database
+                        </div>
+
+                    </div>
+
+                    <div className={styles.separator}/>
+
+                    <div>
+                        Copy Current FEN
+                        <IconButton onClick={onClickCopyFen} sx={{color: 'white','&:hover': 
+                            {backgroundColor: '#545454'}}}>
+                            <ContentCopyIcon sx={{fill: 'white'}}/>
+                        </IconButton> 
+                    </div>
                 </div>
-            </div>
-
-            <div className={styles.separator}/>
-
-            <div className={styles.dbInfo}>
-
-                <div className={styles.numGamesInDB}>
-                    {prettyPrintNumber(numGamesInDB!)}  game(s) in database
-                </div>
-
-      
-                •
-
-                <div className={styles.numMovesInDB}>
-                    {numPossibleMoves} possible move(s) in database
-                </div>
-
-            </div>
-
-            <div className={styles.separator}/>
-
-            <div>
-                Copy Current FEN
-                <IconButton onClick={onClickCopyFen} sx={{color: 'white','&:hover': {backgroundColor: '#545454'}}}>
-                    <ContentCopyIcon sx={{fill: 'white'}}/>
-                </IconButton> 
-            </div>
-        </div>
+            )
+        }
+        </>
+        
+        
     )
 }
